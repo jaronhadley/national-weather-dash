f314c364b4fa126277cdf1ee994bd7ef1d871b7a
Jaron Hadley <jaron.hadley@gmail.com>
2022-07-03 17:49:36 -0600

    initial formatting

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// search city name link
+var searchCity = $("#search-city");
+// today link
+$("#today-date").text(moment().format("M/D/YYYY"));
+// today temp link
+// today wind link
+// today humidity link
+// today UV link
+
+// Forecast Card links
+// day plus one link
+var dayPlusOne = $("#dayPlusOne");
+// day plus two link
+var dayPlusTwo = $("#dayPlusTwo");
+// day plus three link
+var dayPlusThree = $("#dayPlusThree");
+// day plus four link
+var dayPlusFour = $("#dayPlusFour");
+// day plus five link
+var dayPlusFive = $("#dayPlusFive");
+
